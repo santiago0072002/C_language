c7f992a698e387fdc6010af921ead2637837e431
Hector Santiago <52340742+santiago0072002@users.noreply.github.com>
2020-06-24 19:16:38 -0400

    Create if_else1.c
    had to add the closing curly brace.

---
+++ if_else1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>  /* I learned this code by looking at Caleb Curry youtube channel. */
+
+int main()
+{
+	int age; 
+
+	printf("What is your age?   \n");
+	scanf("%d", &age); 
+	
+    if(age < 13)
+    {
+        printf("You're a youngling\n");
+    }
+    else if(age > 12 && age < 20) //NOT  12 < age < 20
+    {
+        printf("You're a teenager\n");
+    } else 
+    {
+        printf("You're an old man\n");
+    }
+	return 0; 
+}
